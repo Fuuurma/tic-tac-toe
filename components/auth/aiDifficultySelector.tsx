9b4a724f5d680883c0602caee7d1c5c49ee95eaa
fuuurma <sergiformatjer1999@gmail.com>
2025-04-10 22:07:30 +0200

    shadcn

---
+++ components/auth/aiDifficultySelector.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const AI_DifficultySelector = () => {
+  return <div>AI_DifficultySelector</div>;
+};
+
+export default AI_DifficultySelector;
